7ff1f247126f43905c19b6694a88c3dfe5a3dd84
Roberto Gomez <rmgomez44@gmail.com>
2019-02-15 11:10:15 -0300

    Create deleteUnattachedManagedDisks.ps1

---
+++ deleteUnattachedManagedDisks.ps1
@@ -0,0 +1,45 @@
+# Set deleteUnattachedVHDs=1 if you want to delete unattached VHDs
+# Set deleteUnattachedVHDs=0 if you want to see the Uri of the unattached VHDs
+$deleteUnattachedVHDs=0
+
+$storageAccounts = Get-AzStorageAccount
+
+foreach($storageAccount in $storageAccounts){
+
+    $storageKey = (Get-AzStorageAccountKey -ResourceGroupName $storageAccount.ResourceGroupName -Name $storageAccount.StorageAccountName)[0].Value
+
+    $context = New-AzureStorageContext -StorageAccountName $storageAccount.StorageAccountName -StorageAccountKey $storageKey
+
+    $containers = Get-AzureStorageContainer -Context $context
+
+    foreach($container in $containers){
+
+        $blobs = Get-AzureStorageBlob -Container $container.Name -Context $context
+
+        #Fetch all the Page blobs with extension .vhd as only Page blobs can be attached as disk to Azure VMs
+        $blobs | Where-Object {$_.BlobType -eq 'PageBlob' -and $_.Name.EndsWith('.vhd')} | ForEach-Object { 
+
+            #If a Page blob is not attached as disk then LeaseStatus will be unlocked
+            if($_.ICloudBlob.Properties.LeaseStatus -eq 'Unlocked'){
+
+                  if($deleteUnattachedVHDs -eq 1){
+
+                        Write-Host "Deleting unattached VHD with Uri: $($_.ICloudBlob.Uri.AbsoluteUri)"
+
+                        $_ | Remove-AzureStorageBlob -Force
+
+                        Write-Host "Deleted unattached VHD with Uri: $($_.ICloudBlob.Uri.AbsoluteUri)"
+                  }
+                  else{
+
+                        $_.ICloudBlob.Uri.AbsoluteUri
+
+                  }
+
+            }
+
+        }
+
+    }
+
+}
